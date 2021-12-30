c144a286dbe011607cbd0a91ce7e5232990db94b
RISHIK DUTTA <rishik.dutta@daimler.com>
2021-12-30 15:45:12 +0530

    update assignment

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# Session 9 assignment of EPAi4.0
+# Session 12 assignment of EPAi4.0
 Named Tuples
 ***
 |Name|Email|Git ID|
 |----|-----|-------|
 |Rishik Dutta|rishikdutta1987@gmail.com|rishikd1987|
 
-## File Name: session9.py
+## File Name: session12.py
 ***
 #### Brief Description:
-Objective 1: Create a Polygon Class where initializer takes in:
+Problem from previous session:
+Objective 1: Create a `Polygon` Class where initializer takes in:
 * number of edges/vertices
 * circumradius
 Class needs to provide these properties:
@@ -23,6 +24,45 @@ Class should provide these functionalities:
 * a proper __repr__ function
 * implements equality (==) based on # vertices and circumradius (__eq__)
 * implements > based on number of vertices only (__gt__)
+Objecttive 2: Implement a Custom Polygon sequence type where initializer takes in:
+* number of vertices for largest polygon in the sequence
+* common circumradius for all polygons
+Following properties need to be provided:
+* max efficiency polygon: returns the Polygon with the highest area: perimeter ratio
+Following properties need to be provided:
+* functions as a sequence type (__getitem__)
+* supports the len() function (__len__)
+* has a proper representation (__repr__)
+Current problem set:
+* Goal 1: Refactor the `Polygon` class so that all the calculated properties are lazy properties,
+i.e. they should still be calculated properties, but they should not have to get recalculated more than once
+(since we made our `Polygon` class "immutable").
 
+* Goal 2: Refactor the `Polygons` (sequence) type, into an **iterable**. Make sure also that the elements in the iterator are computed lazily - 
+i.e. you can no longer use a list as an underlying storage mechanism for your polygons.
+You'll need to implement both an iterable and an iterator.
 #### Packages imported:
 * math
+
+## Code Descriptions
+|Type|Name|Input parameters|Output parameters|Description|Dependency|
+|----|-----|-------|-------|-------|-------|
+|class|`Polygon`|-|-|Polygon class|- |
+|constructor|__init__|Tuple of (edge,circumradius)|-|creates Polygon object - minimum 3 edges required|- |
+|setter|edge|int|-|Setter for edge property / also sets all property values to None for lazy evaluation - interior angle, edge length, apothem, area, perimeter|- |
+|setter|circumradius|int|-|Setter for circumradius property / also sets all property values to None for lazy evaluation - interior angle, edge length, apothem, area, perimeter|- |
+|dunder method|__repr__|-|-|retruns a string representation of class object|- |
+|function|interior_angle|-|-|Calculate interior angle|- |
+|function|edge_length|-|-|Calculate edge length|- |
+|function|apothem|-|-|Calculate apothem|- |
+|function|area|-|-|Calculate area|- |
+|function|perimeter|-|-|Calculate perimeter|- |
+|dunder method|__eq__|-|-|Implements equality property based on edge and circumradius|- |
+|dunder method|__gt__|-|-|implements 'greater than' property between 2 polygon objects|- |
+|class|`Polygon_sequence`|-|-|Polygon Sequence class - `Iterable`|- |
+|constructor|__init__|Largest_edge(int) and common circumradius(int)|-|creates Polygon sequence(Iterable)|- |
+|dunder method|__iter__|-|-|returns PolySeqIter object (Iterator)|- |
+|sub class|`PolySeqIter`|Largest_edge(int) and common circumradius(int)|-|Iterator|- |
+|dunder method|__iter__|-|-|Iter function inside PolySeqIter class|- |
+|dunder method|__next__|-|-|Iterates through the Polygon sequence...stops when it reaches largest edge|- |
+|function|max_efficiency|-|-|prints polygon wth maximum area-perimeter ratio|- |
--- session12.py
+++ session12.py
@@ -17,18 +17,74 @@ import math
 	* a proper __repr__ function
 	* implements equality (==) based on # vertices and circumradius (__eq__)
 	* implements > based on number of vertices only (__gt__)
-"""
+	
+	********************************************************************************************************
+	The starting point for this assignment is the `Polygon` class and the `Polygons` 
+	sequence type you created in the previous assignment.
 
+	The code for these classes along with the unit tests for the `Polygon` class are below 
+	if you want to use those as your starting point. But use whatever you came up with in the last project.
+	You have two goals:
+	Goal 1:
+	Refactor the `Polygon` class so that all the calculated properties are lazy properties, 
+	i.e. they should still be calculated properties, but they should not have to get recalculated more than once 
+	(since we made our `Polygon` class "immutable").
+	 
+	Goal 2:
+	Refactor the `Polygons` (sequence) type, into an **iterable**. 
+	Make sure also that the elements in the iterator are computed lazily - 
+	i.e. you can no longer use a list as an underlying storage mechanism for your polygons.
+	
+	You'll need to implement both an iterable and an iterator.
+"""
 class Polygon:
-	def __init__(self,edge:int,circumradius:int):
+	def __init__(self,val):
 		"""
 			Constructor to create polygon class.
 			Takes 2 parameters - number of edges and circumradius
 		"""
-		if edge<3:
-			raise ValueError("Edge count should be more than 3 to be a valid polygon")
-		self.edge = edge
-		self.circumradius = circumradius
+		try:
+			edge, circumradius = val
+		except ValueError:
+			raise ValueError("Please pass an iterable with 2 items - edge and circumradius")
+		else:
+			if edge<3:
+				raise ValueError("Edge count should be more than 3 to be a valid polygon")
+			self.edge = edge
+			self.circumradius = circumradius
+
+
+	@property
+	def edge(self):
+		"""
+			outputs the number of edges
+		"""
+		return self._edge
+
+	@property
+	def circumradius(self):
+		"""
+			outputs the circumradius
+		"""
+		return self._circumradius
+
+	@edge.setter
+	def edge(self,e):
+		self._edge = e
+		self._interior_angle = None
+		self._edge_length = None
+		self._apothem = None
+		self._area = None
+		self._perimeter = None
+
+	@circumradius.setter
+	def circumradius(self,c):
+		self._circumradius = c
+		self._interior_angle = None
+		self._edge_length = None
+		self._apothem = None
+		self._area = None
+		self._perimeter = None
 
 	def __repr__(self):
 		"""
@@ -36,47 +92,62 @@ class Polygon:
 		"""
 		return f"Polygon with {self.edge} edges and circumradius={self.circumradius}"
 
-	def no_of_edges(self):
-		"""
-			outputs the number of edges
-		"""
-		return self.edge
-
+	@property
 	def no_of_vertices(self):
 		"""
 			prints number of vertices
 		"""
-		return self.edge
+		return self._edge
 
+	@property
 	def interior_angle(self):
 		"""
 			print out the interior angle of the polygon
 		"""
-		return ((self.edge-2)*180/self.edge)
+		if self._interior_angle is None:
+			print("Calculating interior angle")
+			self._interior_angle = ((self._edge-2)*180/self._edge)
+		return self._interior_angle
 
+	@property
 	def edge_length(self):
 		"""
 			prints edge length of polygon
 		"""
-		return (2 * self.circumradius * math.sin(math.pi/self.edge))
+		if self._edge_length is None:
+			print("Calculating edge length")
+			self._edge_length = (2 * self._circumradius * math.sin(math.pi/self._edge))
+		return self._edge_length
 
+	@property
 	def apothem(self):
 		"""
 			prints apothem of the polygon
 		"""
-		return (self.circumradius * math.cos(math.pi/self.edge))
+		if self._apothem is None:
+			print("Calculating apothem")
+			self._apothem = (self._circumradius * math.cos(math.pi/self._edge))
+		return self._apothem
 
+	@property
 	def area(self):
 		"""
 			prints area of the polygon
 		"""
-		return (0.5 * self.edge * self.interior_angle() * self.apothem())
+		if self._area is None:
+			print("Calculating area")
+			self._area = (0.5 * self._edge * self.interior_angle * self.apothem)
+		return self._area
 
+	@property
 	def perimeter(self):
 		"""
 			prints perimeter of the polygon
 		"""
-		return (self.edge * self.edge_length())
+		if self._perimeter is None:
+			print("Calculating perimeter")
+			self._perimeter = (self._edge * self.edge_length)
+		return self._perimeter
 
 	def __eq__(self, other):
 		"""
@@ -84,7 +155,7 @@ class Polygon:
 		"""
 		if not isinstance(other, Polygon):
 			raise TypeError(f"{other} is not of type Polygon")
-		if self.edge == other.edge and self.circumradius == other.circumradius:
+		if self._edge == other._edge and self._circumradius == other._circumradius:
 			return True
 		else:
 			return False
@@ -95,44 +166,47 @@ class Polygon:
 		"""
 		if not isinstance(other, Polygon):
 			raise TypeError(f"{other} is not of type Polygon")
-		if self.edge > other.edge:
+		if self._edge > other._edge:
 			return True
 		else:
 			return False
 
 class Polygon_sequence:
 	def __init__(self,largest_edge,common_circumradius):
-		"""
-			constructor to create polygon sequence
-		"""
 		self.largest_edge = largest_edge
 		self.common_circumradius = common_circumradius
-		self.polygon_seq = [Polygon(x,self.common_circumradius) for x in range(3,self.largest_edge+1)]
 
-	def __repr__(self):
-		"""
-			prints a string representation of a plygon_sequence class object
-		"""
-		return str(self.polygon_seq)
+
+	def __iter__(self):
+		return self.PolySeqIter(self.largest_edge,self.common_circumradius)
+
+	class PolySeqIter:
+		def __init__(self, largest_edge,common_circumradius):
+			self.largest_edge = largest_edge
+			self.common_circumradius = common_circumradius
+			self.i = 3
+
+		def __iter__(self):
+			return self
+
+		def __next__(self):
+			if self.i > self.largest_edge:
+				raise StopIteration
+			else:
+				result = Polygon((self.i,self.common_circumradius))
+				self.i += 1
+				return result
 
 	def max_efficiency(self):
 		"""
 			prints polygon wth maximum area-perimeter ratio
 		"""
-		eff_list = [(x.area()/x.perimeter()) for x in self.polygon_seq]
-		return self.polygon_seq[eff_list.index(max(eff_list))]
+		eff_list = [(x.area/x.perimeter) for x in list(self)]
+		return list(self)[eff_list.index(max(eff_list))]
 
-	def __len__(self):
-		"""
-			returns number of polygons in the sequence
-		"""
-		return len(self.polygon_seq)
 
-	def __getitem__(self, item):
-		"""
-			implement slicing operation in polygon_sequence object
-		"""
-		return self.polygon_seq[item]
+
+
 
 
 
